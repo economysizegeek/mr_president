7e837d44e40b601199282b30bf2f25fd3c200a3c
Dirk Elmendorf <delmendo@abbot12.(none)>
2010-01-05 17:01:38 -0600

    Still working on an election

--- mr_president.clj
+++ mr_president.clj
@@ -1,19 +1,33 @@
 
 (def politician1 {
+   :id 1
    :charisma 45.0
    :issues 10.0
    :familiarity 45.0 })
 
 (def politician2 {
+   :id 2
    :charisma 12.0
    :issues 75.0 
    :familiarity 13.0 })
 
 (def voter1 {
+   :id 1
    :charisma 0.50
    :issues 0.0
    :familiarity 0.50
    })
+
+(def voter2 {
+   :id 2
+   :charisma 0.50
+   :issues 0.0
+   :familiarity 0.50
+   })
+
+(defn sort_by_score [a b]
+ ( > (a :score) (b :score))
+)
 
 (defn score [voter politician]
  ( + ( * ( voter :charisma) ( politician :charisma))
@@ -21,16 +35,30 @@
      ( * ( voter :familiarity) ( politician :familiarity))
     ))
   
-(def v1_p1 
-  (score voter1 politician1)
-)
-(def v1_p2 
-  (score voter1 politician2)
-)
 (defn vote [voter politicians]
- (score voter (first politicians)) 
- (recur score [voter (next politicians)])
+ (defn v_score [p] ( merge p {:score (score voter p)}))
+ (first (sort sort_by_score (map v_score politicians)))
 )
 
-(vote voter1 [politician1, politician2])
+(defn election [voters politicians]
+ (defn v_vote [v] ( vote v politicians))
+ (defn count_by_id [id p_list] (count (filter (fn [item] (= id (item :id))) p_list)))
+ (defn politician_by_id [id] (filter (fn [item] (= id (item :id))) politicians))
+
+ (let [results 
+    (loop [ result [] vs voters ]
+    (if (empty? vs)
+     result
+    (recur (conj result (v_vote (first vs))) (rest vs)))
+   )] 
+   (defn get_id [item] (item :id))
+   (defn list-of-ids [] (map get_id politicians))
+   (defn politician_with_votes [id] 
+    (merge ( first (politician_by_id id) ) { :votes (count_by_id id results)})
+   )
+   (map politician_with_votes (distinct(list-of-ids)))
+ )
+)
+
+(election [voter1 voter2] [politician1, politician2])
 
