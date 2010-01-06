49ce48936e34d8c36f6ada1cae2659a4557d8c02
Dirk Elmendorf <delmendo@guinness.(none)>
2010-01-06 07:40:52 -0600

    Adding in other notes

---
+++ election.clj
@@ -0,0 +1,45 @@
+#This is the first test of the election ideas
+
+#You get politicians
+
+#Characteristice:
+
+#Charisma - Can be improved by spending money on better speech writers
+#Familiarity - Can be improved by spending money on more advertisinug
+#Issues - Can be improved by hiring better epxerts
+#Orthodoxy - Cannot be changed
+
+#Politicians can also spend money on:
+#Look for scandal - counter to charisma
+#Attach Ads - counter to issues
+
+#Voters have a weighted preference for each of the three characteristics modified by any negative attacks from the opponten and improved by matching orthodoxy.
+
+#Thin Vertical Slice
+
+#Build a Voter
+#Collect 10 Voters
+#Build a Politician
+#Collect 2 Politicians
+#run an election with voters and politician
+#Declare a winner 
+ 
+(defn build_voter 
+"This function generates a voter"
+)
+
+(defn build_electorate
+"This function creates a list of voters"
+)
+
+(defn build_politician
+"This function creates a random politician"
+)
+
+(defn build_ballot
+"This function creates a list of politicians"
+)
+
+(defn election
+"This function takes a ballot and an electorate and determines the winner"
+)
